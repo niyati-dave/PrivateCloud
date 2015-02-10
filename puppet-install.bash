bb1ccc548ac8f8701ff4ec387e0723bf25dd0eed
Niyati Dave <niyati.dave@ril.com>
2015-02-10 16:20:06 +0530

    add new file

--- puppet-install.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -e use_proxy=yes -e https_proxy=10.135.80.164:8678 https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-dpkg -i puppetlabs-release-trusty.deb
-apt-get install puppet -y
-puppet module install maestrodev-ssh_keygen
-
